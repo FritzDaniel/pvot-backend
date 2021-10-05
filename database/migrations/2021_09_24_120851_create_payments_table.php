e974af9577df9f75ee58f7c068fe157b85ab9f4c
Fritz <danielsinaga53@gmail.com>
2021-10-05 17:13:45 +0700

    add user id to payment table

--- database/migrations/2021_09_24_120851_create_payments_table.php
+++ database/migrations/2021_09_24_120851_create_payments_table.php
@@ -16,6 +16,7 @@ class CreatePaymentsTable extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->string('external_id');
+            $table->integer('user_id');
             $table->string('payment_channel');
             $table->string('email');
             $table->double('price');
