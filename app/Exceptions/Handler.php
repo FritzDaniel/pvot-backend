484e1e660732b657f624cfee086aac1f03a0c48d
Fritz <danielsinaga53@gmail.com>
2021-10-10 13:30:31 +0700

    response status token expired

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +38,20 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(
+                [
+                    'status' => false,
+                    'status_code' => 401,
+                    'data' => [
+                        'message' => 'Unauthenticated'
+                    ]
+                ], 401
+            );
+        }
     }
 }
