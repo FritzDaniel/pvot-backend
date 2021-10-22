f1655dadc00706db9dda2dec6f2dc31c3fe518ec
Fritz <danielsinaga53@gmail.com>
2021-10-22 08:27:28 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(RoleSeeder::class);
-        //$this->call(SettingsSeeder::class);
-        $this->call(VariantSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(SettingsSeeder::class);
+//        $this->call(VariantSeeder::class);
     }
 }
