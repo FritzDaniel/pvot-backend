c2b4a1c14627340d01b5c90c64e442d99c834ca5
Fritz <danielsinaga53@gmail.com>
2021-11-04 21:34:41 +0700

    sidebar fix

--- resources/views/supplier/layouts/partials/sidebar.blade.php
+++ resources/views/supplier/layouts/partials/sidebar.blade.php
@@ -10,7 +10,7 @@
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ Auth::user()->profilePicture }}" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ asset(Auth::user()->profilePicture) }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block">{{ Auth::user()->name }}</a>
@@ -27,6 +27,15 @@
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Dashboard
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-header">Dropshipper</li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-user"></i>
+                        <p>
+                            Dropshipper List
                         </p>
                     </a>
                 </li>
--- resources/views/supplier/withdraw/index.blade.php
+++ resources/views/supplier/withdraw/index.blade.php
@@ -45,7 +45,7 @@
 
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">No Rek List</h3>
+                        <h3 class="card-title">Account Number List</h3>
 
                         <div class="card-tools">
                             <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
