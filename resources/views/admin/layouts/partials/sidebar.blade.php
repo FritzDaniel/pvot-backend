40c875e74370f0554a370220a59087ab34a73ea2
Fritz <danielsinaga53@gmail.com>
2021-11-13 15:34:46 +0700

    tambahan

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -1,8 +1,9 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="https://pvotdigital.com/" class="brand-link elevation-4 text-center">
-        <span class="brand-text font-weight-light"><strong>PVOT</strong> DIGITAL</span>
+    <a href="https://pvotdigital.com/" class="brand-link">
+        <img src="{{ asset('logo512.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light"><strong>PVOT</strong> Digital</span>
     </a>
 
     <!-- Sidebar -->
